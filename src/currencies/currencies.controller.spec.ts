14f1d1da72ab68e7d414d2a8f3780354f24baf50
盧偉嘉 <jeremy60607@gmail.com>
2021-12-06 01:13:17 +0800

    fix: add unit test and e2e test

--- src/currencies/currencies.controller.spec.ts
+++ src/currencies/currencies.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CurrenciesController } from './currencies.controller';
+import { CurrenciesService } from './currencies.service';
 
 describe('CurrenciesController', () => {
   let controller: CurrenciesController;
@@ -7,6 +8,7 @@ describe('CurrenciesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CurrenciesController],
+      providers: [CurrenciesService],
     }).compile();
 
     controller = module.get<CurrenciesController>(CurrenciesController);
--- src/currencies/currencies.service.spec.ts
+++ src/currencies/currencies.service.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CurrenciesService } from './currencies.service';
+import { ExchangeCurrencyBodyDTO } from './dto/currency.dto';
+import { BadRequestException } from '@nestjs/common';
+import { ExchangeRatePO } from './po/currency.po';
+import { ExceptionConst } from '../const/exception.const';
 
 describe('CurrenciesService', () => {
   let service: CurrenciesService;
@@ -14,5 +18,46 @@ describe('CurrenciesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('check country is invalid', () => {
+    const { COUNTRY_IS_INVALID } = ExceptionConst;
+    const checkCountryFunction = () => {
+      service.fetchExchangeRate('T', 'USD');
+    };
+
+    expect(checkCountryFunction).toThrow(BadRequestException);
+    expect(checkCountryFunction).toThrow(COUNTRY_IS_INVALID);
+  });
+
+  it('check exchangeCountry is invalid', () => {
+    const { COUNTRY_IS_INVALID } = ExceptionConst;
+    const checkCountryFunction = () => {
+      service.fetchExchangeRate('TWD', 'U');
+    };
+
+    expect(checkCountryFunction).toThrow(BadRequestException);
+    expect(checkCountryFunction).toThrow(COUNTRY_IS_INVALID);
+  });
+
+  it('fetchExchangeRate success', () => {
+    const po: ExchangeRatePO = { rate: 111.801 };
+
+    expect(service.fetchExchangeRate('USD', 'JPY')).toEqual(po);
+  });
+
+  it('calculateCurrency success', () => {
+    const po: ExchangeRatePO = { rate: 111.801 };
+
+    expect(service.fetchExchangeRate('USD', 'JPY')).toEqual(po);
+  });
+
+  it('addThousandsSeparatorAndRoundingDigits success', () => {
+    expect(
+      service.addThousandsSeparatorAndRoundingDigits(31555125.8456),
+    ).toEqual('31,555,125.85');
+    expect(
+      service.addThousandsSeparatorAndRoundingDigits(31555125.8446),
+    ).toEqual('31,555,125.84');
   });
 });
