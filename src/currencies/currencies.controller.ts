709283d625202ccfeccb860af9500c6965e82c8d
盧偉嘉 <jeremy60607@gmail.com>
2021-12-06 01:12:27 +0800

    feat: implementation api endpoint

--- src/currencies/currencies.controller.ts
+++ src/currencies/currencies.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put } from '@nestjs/common';
+import { ExchangeCurrencyBodyDTO } from './dto/currency.dto';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { CurrenciesService } from './currencies.service';
+import { ExchangeCurrencyAO } from './ao/currency.ao';
 
-@Controller('currencies')
-export class CurrenciesController {}
+@ApiTags('v1/currencies')
+@Controller('v1/currencies')
+export class CurrenciesController {
+  constructor(private readonly currenciesService: CurrenciesService) {}
+
+  @ApiOkResponse({
+    type: () => ExchangeCurrencyAO,
+  })
+  @Put('/exchange')
+  exchangeCurrency(@Body() body: ExchangeCurrencyBodyDTO) {
+    return this.currenciesService.exchangeCurrency(body);
+  }
+}
